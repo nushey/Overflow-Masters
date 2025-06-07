0fce5114ab856ff253dd9c2b8218a36d7cab29a5
nushey <zeballosnahuelfd@gmail.com>
2025-06-07 16:26:20 -0300

    fixs

--- src/Graphs/bellman.cpp
+++ src/Graphs/bellman.cpp
@@ -1,27 +1,19 @@
-vec<int> G[N];
-vec<int> dfs_low(N, -1), dfs_num(N, -1), ap(N, 0); // ap for Articulation Points
-int dfs_count = 0;
-int root = -1; // For AP
-void dfs(int u, int p = -1){
-    dfs_low[u]=dfs_num[u]=dfs_count++;
-    int child = 0;
-    for (int v: G[u]){
-        if (v == p) continue;
-        if (dfs_num[v] == -1){
-            child ++;
-            dfs(v, u);
-            dfs_low[u] = min(dfs_low[u], dfs_low[v]);
-            if (dfs_low[v] > dfs_num[u]){
-              // Bridge from u -> v
-              cout << "Bridge " << u << " -> " << v << "\n";
-            } 
-            if (dfs_low[v] >= dfs_num[u]) {
-                // u is AP
-                ap[u] = 1;
+void bellmanFord(int n, int source, vec<vec<pii>> &g, vec<int> &d)
+{
+    d.assign(n, INT_MAX);
+    d[source] = 0;
+
+    for (int i = 0; i < n - 1; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            for (auto &[a, c] : g[j])
+            {
+                if (d[j] != INT_MAX && d[a] > d[j] + c)
+                {
+                    d[a] = d[j] + c;
+                }
             }
-        } else dfs_low[u] = min(dfs_low[u], dfs_num[v]);
-    }
-    if (u == root){
-        ap[u] = child > 1;
+        }
     }
 }
--- src/Graphs/ufds.cpp
+++ src/Graphs/ufds.cpp
@@ -1,83 +1,34 @@
-#include <bits/stdc++.h>
-#define ll long long
-#define MOD 1000000007
-using namespace std;
+class UFDS
+{
+private:
+    vi p, rank, setSize;
+    int numSets;
 
-const int tam = 20000;
-
-vector<int> father(tam);
-vector<int> sizes(tam, 1);
-
-int find(int node) {
-    if (father[node] != node) {
-        father[node] = find(father[node]);
+public:
+    UFDS(int N)
+    {
+        p.assign(N, 0);
+        for (int i = 0; i < N; i++)
+            p[i] = i;
+        rank.assign(N, 0);
+        setSize.assign(N, 1);
+        numSets = N;
     }
-    return father[node];
+    int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
+    bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
+    int numDisjointSets() { return numSets; }
+    int sizeOfSet(int i) { return setSize[findSet(i)]; }
+    void unionSet(int i, int j)
+    {
+        if (isSameSet(i, j))
+            return;
+        int x = findSet(i), y = findSet(j);
+        if (rank[x] > rank[y])
+            swap(x, y);
+        p[x] = y;
+        if (rank[x] == rank[y])
+            ++rank[y];
+        setSize[y] += setSize[x];
+        --numSets;
+    }
 };
-
-void unions(int a, int b) {
-    if (sizes[a] > sizes[b]) {
-        father[b] = a;
-        sizes[a] += sizes[b];
-    }
-    else {
-        father[a] = b;
-        sizes[b] += sizes[a];
-    }
-}
-
-int main() {
-    int n, m;
-    cin >> n >> m;
-    
-    for (int i = 1; i <= n; i++) {
-        father[i] = i;
-    }
-    int groups = n;
-    int maxSize = 1;
-    while (m-- > 0) {
-        int a, b; cin >> a >> b;
-        int fatherA = find(a);#include <bits/stdc++.h>
-
-        using namespace std;
-        
-        typedef vector<int> vi;
-        
-        
-        class UFDS{
-            private:
-                vi p, rank, setSize;
-                int numSets;
-            public:
-                UFDS(int N){
-                    p.assign(N,0); for(int i = 0; i < N; i++) p[i] = i;
-                    rank.assign(N,0);
-                    setSize.assign(N, 1);
-                    numSets = N;
-                }
-                int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i]));}
-                bool isSameSet(int i, int j) { return findSet(i) == findSet(j);}
-                int numDisjointSets() { return numSets;}
-                int sizeOfSet(int i) { return setSize[findSet(i)];}
-                void unionSet(int i, int j){
-                    if(isSameSet(i,j)) return;
-                    int x = findSet(i), y = findSet(j);
-                    if(rank[x] > rank[y]) swap(x,y);
-                    p[x] = y;
-                    if(rank[x] == rank[y]) ++rank[y];
-                    setSize[y] += setSize[x];
-                    --numSets;
-                }
-        };
-        
-        
-        int fatherB = find(b);
-        if (fatherA != fatherB) {
-            unions(fatherA, fatherB);
-            maxSize = max(maxSize, sizes[find(fatherA)]);
-            groups--;
-        }
-        cout << groups << " " << maxSize << endl;
-    }
-    return 0;
-}
