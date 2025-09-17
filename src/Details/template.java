bb0f6d6b4c0a842bf47c37531a085a50010d7040
Nahuel <zeballosnahuelfd@gmail.com>
2025-09-16 23:34:58 -0300

    big int & template java

---
+++ src/Details/template.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.ArrayList; 
+import java.util.Collections; 
+
+public class Main {
+
+    static final int MOD = 1_000_000_007; 
+    
+    static void solve(Scanner sc) {
+        
+        // int n = sc.nextInt();
+        // long k = sc.nextLong();
+        // string s = sc.next();
+        
+        // System.out.println("El resultado es: " + (n + k));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            solve(sc);
+        }
+
+        sc.close();
+    }
+}
--- src/Search/count_bits.cpp
+++ src/Search/count_bits.cpp
@@ -1,7 +1,15 @@
-ll count_beauty(const vec<ll>& a) {
-    ll total_beauty = 0;
+ll count(const vec<ll>& a) {
+    ll sum = 0;
     for (ll x : a) {
-        total_beauty += __builtin_popcountll(x);
+        sum += __builtin_popcountll(x);
     }
-    return total_beauty;
+    return sum;
+}
+// ANOTHER IMPLEMENTATION
+ll ans = 0;
+L(i, 0, n) {
+    cin >> a[i];
+    L(bit, 0, 62) {
+        if (a[i] & (1ll << bit)) ans++;
+    }
 }
